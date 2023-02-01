d8f16e0b01f34ff73ab1d9ff4a48d43b68657ca4
jack <Ciao@lazych.me>
2023-02-01 01:39:01 +0100

    ready to production

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,27 @@
-export default function Home() {
+import { useSession, signIn, signOut } from "next-auth/react"
+
+export default function CamperVanPage() {
+  const { data: session, status } = useSession()
+  const userEmail = session?.user.name
+
+  if (status === "loading") {
+    return <p>Hang on there...</p>
+  }
+
+  if (status === "authenticated") {
+    return (
+      <>
+        <p>Hello {userEmail}</p>
+        <button onClick={() => signOut()}>Sign out</button>
+        <img src="https://cdn.pixabay.com/photo/2017/08/11/19/36/vw-2632486_1280.png" />
+      </>
+    )
+  }
+
   return (
     <>
-      <main>hello</main>
+      <p>Not signed in.</p>
+      <button onClick={() => signIn()}>Sign in</button>
     </>
   )
 }
